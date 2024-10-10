db2fa3821808074f55b3f2a56ef0752f3df807da
abobakr.awamey <abm548353@gmail.com>
2024-10-10 16:56:12 +0300

    first problem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,46 +4,49 @@ public class Main {
 
     public static void main(String[] args) {
 
-               /* V. Comparison   */
-
-
-
-
-
                 Scanner scanner = new Scanner(System.in);
 
                 // Declare variables
-                int A, B;
-                char C;
+                long A, B, C;
 
-                // Read inputs A, C (comparison symbol), and B
-                A = scanner.nextInt();
-                C = scanner.next().charAt(0);
-                B = scanner.nextInt();
+                // Read inputs A, B, and C
+                A = scanner.nextLong();
+                B = scanner.nextLong();
+                C = scanner.nextLong();
 
-                // Perform the comparison based on the value of C
-                if (C == '<') {
-                    if (A < B) {
-                        System.out.println("Right");
+                // Check if A is the largest
+                if (A >= B && A >= C) {
+                    if (B > C) {
+                        System.out.println(C + " " + A);
                     } else {
-                        System.out.println("Wrong");
-                    }
-                } else if (C == '>') {
-                    if (A > B) {
-                        System.out.println("Right");
-                    } else {
-                        System.out.println("Wrong");
-                    }
-                } else if (C == '=') {
-                    if (A == B) {
-                        System.out.println("Right");
-                    } else {
-                        System.out.println("Wrong");
+                        System.out.println(B + " " + A);
                     }
                 }
 
+                // Check if B is the largest
+                else if (B >= A && B >= C) {
+                    if (A > C) {
+                        System.out.println(C + " " + B);
+                    } else {
+                        System.out.println(A + " " + B);
+                    }
+                }
 
+                // Check if C is the largest
+                else if (C >= A && C >= B) {
+                    if (A > B) {
+                        System.out.println(B + " " + C);
+                    } else {
+                        System.out.println(A + " " + C);
+                    }
+                }
+
+                // Close the scanner
+                scanner.close();
             }
         }
+
+
+
 
 
